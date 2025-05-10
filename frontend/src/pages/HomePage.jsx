4084963b045f6fbeae80a412ff2841fd687851dc
Cuong Dao S. <cuong.dao@monstar-lab.com>
2025-05-10 20:55:14 +0700

    WIP: thay đổi tạm thời trước khi merge main

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function HomePage() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:5000/')
-      .then(res => setMessage(res.data))
-      .catch(err => console.error(err));
+    axios
+      .get("http://localhost:5000/")
+      .then((res) => setMessage(res.data))
+      .catch((err) => console.error(err));
   }, []);
 
   return <div>Home Page</div>;
